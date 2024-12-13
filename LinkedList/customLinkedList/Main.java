306b847f874c8e532a2e031bced2cb17133a3f16
RizWaaN3024 <rizwaanans786@gmail.com>
2024-12-14 01:53:01 +0530

    Insert function and display function

--- LinkedList/customLinkedList/Main.java
+++ LinkedList/customLinkedList/Main.java
@@ -1,8 +1,16 @@
 package LinkedList.customLinkedList;
 
+import LinkedList.customLinkedList.LinkedList;
 public class Main {
     public static void main(String[] args) {
 
         LinkedList list = new LinkedList();
+        list.insertFirst(18);
+        list.insertFirst(20);
+        list.insertFirst(12);
+        list.insertFirst(6);
+        list.insertFirst(5);
+
+        list.display();
     }
 }
