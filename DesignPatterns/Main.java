c9e92289fd9b867b2783fecd757131713c058a40
RizWaaN3024 <rizwaanans786@gmail.com>
2024-12-17 02:31:59 +0530

    Constructors methods

--- DesignPatterns/Main.java
+++ DesignPatterns/Main.java
@@ -3,9 +3,7 @@ package DesignPatterns;
 public class Main {
     
     public static void main(String[] args) {
-        User user = new User();
-
-        user.name = "Rizwaan";
-        System.out.println(user.name);
+        User user = new User("Rizwaan");
+        user.sayHello();
     }
 }
--- DesignPatterns/User.java
+++ DesignPatterns/User.java
@@ -4,7 +4,14 @@ public class User {
     
     // Fields (attributes)
     public String name;
+    // constructor-- method which gets called when an object is created
 
-    // Methods
+    public User(String name) {
+        this.name = name;
+    }
 
+    // methods
+    public void sayHello() {
+        System.out.println("Hi, my name is" + name);
+    }
 }
