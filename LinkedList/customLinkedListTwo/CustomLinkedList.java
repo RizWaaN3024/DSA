3216ce971f159a7fa2b239f4fe5ff673f4930c2d
RizWaaN3024 <rizwaanans786@gmail.com>
2024-12-27 02:14:56 +0530

    Remove element by value

--- LinkedList/customLinkedListTwo/CustomLinkedList.java
+++ LinkedList/customLinkedListTwo/CustomLinkedList.java
@@ -43,4 +43,27 @@ public class CustomLinkedList<T> {
         size++;
     }
 
+    // removing an element by value
+    public boolean remove(T data) {
+        if (head == null) return false;
+
+        if (head.data.equals(data)) {
+            head = head.next;
+            size--;
+            return true;
+        }
+
+        Node<T> current = head;
+        while (current.next != null && !current.next.data.equals(data)) {
+            current = current.next;
+        }
+
+        if (current.next != null) {
+            current.next = current.next.next;
+            size--;
+            return true;
+        }
+        return false;
+    }
+
 }
