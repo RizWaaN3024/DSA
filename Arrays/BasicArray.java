7cace442a9322f45955130341e7dd2f33cfdf323
RizWaaN3024 <rizwaanans786@gmail.com>
2024-12-29 01:25:19 +0530

    Arrays

---
+++ Arrays/BasicArray.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class BasicArray {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[5];
+        
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+    
+        for (int num : arr) {
+            System.out.println(num);
+        }
+    }
+    
+}
