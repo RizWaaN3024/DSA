25339e3ca377b0ccb23dcfe616fb841a636d9b9e
RizWaaN3024 <rizwaanans786@gmail.com>
2024-12-29 23:25:03 +0530

    ArrayList example

--- Arrays/ArrayListExample.java
+++ Arrays/ArrayListExample.java
@@ -1,23 +1,48 @@
 package Arrays;
 
 import java.util.ArrayList;
+import java.util.*;
 
 public class ArrayListExample {
     // Syntax of an arraylist
     // ArrayList<Datatype> name = new ArrayList<>();
+
+    public void inputArrayListElements(ArrayList<Integer> list) 
+    {
+        Scanner in = new Scanner(System.in);
+        for(int i = 0; i < 5; i++) {
+            list.add(in.nextInt());
+        }
+        in.close();
+    }
+
+    public void displayArrayList(ArrayList<Integer> list) 
+    {
+        System.out.println(list);
+    }
+
+    public void getElementAtIndex(int index, ArrayList<Integer> list)
+    {
+        System.out.println(list.get(index));
+    }
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<>(10);
+        ArrayListExample obj = new ArrayListExample();
 
-        arrayList.add(100);
-        arrayList.add(140);
-        arrayList.add(160);
-        arrayList.add(110);
-        arrayList.add(130);
-        arrayList.add(120);
-        arrayList.add(20);
+        obj.inputArrayListElements(arrayList);
+        obj.displayArrayList(arrayList);
 
-        System.out.println(arrayList);
-        System.out.println(arrayList.contains(100));
+        // arrayList.add(100);
+        // arrayList.add(140);
+        // arrayList.add(160);
+        // arrayList.add(110);
+        // arrayList.add(130);
+        // arrayList.add(120);
+        // arrayList.add(20);
+
+        // arrayList.set(0, 99);
+        // System.out.println(arrayList);
+        // System.out.println(arrayList.contains(100));
     }
 
 }
