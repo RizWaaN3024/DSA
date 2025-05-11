a28438b21e4f441a191a870005a025db537de6d7
RizWaaN3024 <rizwaanans786@gmail.com>
2025-05-11 18:09:19 +0530

    Linked List

--- LinkedList/customLinkedListTwo/CustomLinkedList.java
+++ LinkedList/customLinkedListTwo/CustomLinkedList.java
@@ -1,80 +1,59 @@
 package LinkedList.customLinkedListTwo;
 
-public class CustomLinkedList<T> {
-    public class Node<T> {
-        T data;
-        Node<T> next;
+public class CustomLinkedList {
+    Node head;
+    class Node {
+        String data;
+        Node next;
 
-        public Node(T data) {
+        Node(String data) {
             this.data = data;
             this.next = null;
         }
     }
-    private Node<T> head;
-    private int size;
-
-    public CustomLinkedList() {
-        head = null;
-        size = 0;
-    }
-
-    // Adding an element to the end of the linkedlist (append)
-    public void append(T data) {
-        Node<T> newNode = new Node<>(data);
-
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
         if (head == null) {
             head = newNode;
-        } else {
-            Node<T> current = head;
-            while (current.next != null) {
-                current = current.next;
-            }
-            current.next = newNode;
+            return;
         }
-        size++;
-    }
-
-    // Adding an element to the begining (prepend)
-    public void prepend(T data) {
-        Node<T> newNode = new Node<>(data);
-
         newNode.next = head;
         head = newNode;
-        size++;
     }
 
-    // removing an element by value
-    public boolean remove(T data) {
-        if (head == null) return false;
-
-        if (head.data.equals(data)) {
-            head = head.next;
-            size--;
-            return true;
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
         }
-
-        Node<T> current = head;
-        while (current.next != null && !current.next.data.equals(data)) {
-            current = current.next;
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
         }
-
-        if (current.next != null) {
-            current.next = current.next.next;
-            size--;
-            return true;
-        }
-        return false;
+        currNode.next = newNode;
     }
 
-    // print the list
-    public void display() {
-        Node<T> current = head;
-
-        while(current != null) {
-            System.out.print(current.data + "->");
-            current = current.next;
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is Empty");
+            return;
         }
-        System.out.println(null);
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + "->");
+            currNode = currNode.next;
+        }
+        System.out.print("null");
     }
-
+    public static void main(String[] args) {
+        CustomLinkedList list = new CustomLinkedList();
+        list.addFirst("u");
+        list.addFirst("are");
+        list.addFirst("How");
+        list.printList();
+        System.out.println();
+        list.addLast("Hello");
+        list.printList();
+    }
 }
--- LinkedList/customLinkedListTwo/Main.java
+++
@@ -1,7 +0,0 @@
-package LinkedList.customLinkedListTwo;
-
-public class Main {
-    public static void main(String[] args) {
-        
-    }
-}
