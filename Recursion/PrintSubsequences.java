2ac5a9fe9bdadbcd3a23e63d64607535b7bda98d
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-01 20:23:47 +0530

    Print all subsequences with sum equal to k using recursion

--- Recursion/PrintSubsequences.java
+++ Recursion/PrintSubsequences.java
@@ -1,29 +1,32 @@
 import java.util.*;
 
-public class PrintSubsequences {
-    
-    private static void printAllSubsequences(int index, ArrayList<Integer> current, int[] arr) {
+public class PrintSubsequences
+{
+
+    private static void printAllSubsequences(int index, ArrayList<Integer> current, int[] arr)
+    {
         if (index >= arr.length) {
             System.out.println(current);
             return;
         }
 
-        // Take: Include Current Element
+        // Take Part
         current.add(arr[index]);
         printAllSubsequences(index + 1, current, arr);
 
-        // Not Take: Exclude Current Element Backtrack first
-        current.remove(current.size() - 1); //Remove last added element
+        // Not Take part
+        current.remove(current.size() - 1); //Remove the last
         printAllSubsequences(index + 1, current, arr);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int arr[] = {3, 1, 2};
+
+        int arr[] = {1, 2, 3};
+
         ArrayList<Integer> list = new ArrayList<Integer>();
 
-        System.out.println("All Subsequences: ");
+        System.out.println("The valid Subsequences are: ");
         printAllSubsequences(0, list, arr);
         sc.close();
     }
---
+++ Recursion/PrintSubsequencesWithSumK.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class PrintSubsequencesWithSumK {
+    
+    private static void PrintSubsequencesWithSumEqualToK(int index, ArrayList<Integer> current, int[] arr, int sum, int k)
+    {
+        if (index == arr.length) {
+            if (k == sum) {
+                System.out.println(current);
+            }
+            return;
+        }
+        // Take part
+        current.add(arr[index]);
+        sum += arr[index];
+        PrintSubsequencesWithSumEqualToK(index + 1, current, arr, sum, k);
+
+        // Not take part
+        sum -= arr[index];
+        current.remove(current.size() - 1);
+        PrintSubsequencesWithSumEqualToK(index + 1, current, arr, sum, k);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = {1, 2, 1};
+        int sum = 0;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        System.out.println("Enter the value of K: ");
+        int k = sc.nextInt();
+        System.out.println("The Subsequences with sum K are: ");
+        PrintSubsequencesWithSumEqualToK(0, list, arr, sum, k);
+    }
+}
