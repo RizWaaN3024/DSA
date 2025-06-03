20a98e49c32186fd60b92f5ab421b014cf879055
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-03 15:55:23 +0530

    Print Subsequences

--- Recursion/PrintSubsequences.java
+++ Recursion/PrintSubsequences.java
@@ -5,29 +5,26 @@ public class PrintSubsequences
 
     private static void printAllSubsequences(int index, ArrayList<Integer> current, int[] arr)
     {
-        if (index >= arr.length) {
+       if (index >= arr.length) {
             System.out.println(current);
             return;
-        }
+       }
 
-        // Take Part
+        //    Take part
         current.add(arr[index]);
         printAllSubsequences(index + 1, current, arr);
 
-        // Not Take part
-        current.remove(current.size() - 1); //Remove the last
+        // Not take part
+        current.remove(current.size() - 1);
         printAllSubsequences(index + 1, current, arr);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int arr[] = {1, 2, 3};
-
+        int arr[] = {1, 2, 1};
         ArrayList<Integer> list = new ArrayList<Integer>();
-
-        System.out.println("The valid Subsequences are: ");
-        printAllSubsequences(0, list, arr);
-        sc.close();
+        System.out.println("The subsequences are: ");
+         printAllSubsequences(0, list, arr);
     }
 }
