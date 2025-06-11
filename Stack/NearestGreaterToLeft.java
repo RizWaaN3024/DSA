f95621319783ecf18ca0a01f5f1e267a7d4d4706
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-11 09:56:34 +0530

    Nearest Greater to left

---
+++ Stack/NearestGreaterToLeft.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class NearestGreaterToLeft {
+    private static List<Integer> findNearestGreaterToLeft(int[] arr)
+    {
+        Stack<Integer> st = new Stack<>();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (st.size() == 0) {
+                list.add(-1);
+            } else if (st.size() > 0 && st.peek() > arr[i]) {
+                list.add(st.peek);
+            } else if (st.size() > 0 && st.peek() <= arr[i]) {
+                while (st.size() > 0 && st.peek() <= arr[i]) {
+                    st.pop();
+                }
+                if (st.size() == 0) {
+                    list.add(-1);
+                } else {
+                    list.add(st.peek());
+                }
+            }
+            st.push(arr[i]);
+        }
+    }
+    public static void main(String[] args)
+    {
+        int arr[] = {1, 3, 2, 4};
+        List<Integer> result = findNearestGreaterToLeft(arr);
+        System.out.println(result);
+    }
+}
