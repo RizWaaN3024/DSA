3a3bad7b90138a3bbe6fea709a2a1644d562f199
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-14 02:38:11 +0530

    Min Stack

--- Stack/MinStackBrute.java
+++ Stack/MinStackBrute.java
@@ -1,3 +1,30 @@
+import java.util.*;
+
 public class MinStackBrute {
     
+    public class Pair {
+        int value;
+        int minValue;
+
+        public Pair(int value, int minValue) {
+            this.value = value;
+            this.minValue = minValue;
+        }
+    }
+    Stack<Pair> st;
+    public MinStackBrute() {
+        st = new Stack<>();
+    }
+
+    void push(int val) {
+        if (st.isEmpty()) {
+            st.push(new Pair(val, val));
+        } else {
+            st.push(new Pair(val, Math.min(val, st.peek().minValue)));
+        }
+    }
+
+    void pop() {
+        st.pop();
+    }
 }
