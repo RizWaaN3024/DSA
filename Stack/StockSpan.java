ed32f6ee02a9e369e713ffa5e1ba401253642981
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-13 12:57:16 +0530

    Update

--- Stack/StockSpan.java
+++ Stack/StockSpan.java
@@ -1,3 +1,21 @@
+import java.util.*;
+
 public class StockSpan {
+
+    private static List<Integer> findStockSpan(int[] arr) 
+    {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (st.isEmpty()) {
+            }
+        }
+    }
     
+    public static void mian(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = {100, 80, 60, 70, 60, 75, 85};
+
+        findStockSpan(arr);
+    }
 }
