32a272e8c52af4102006cc02b83ebbd056cae4ab
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-13 13:21:23 +0530

    Stock Span Brute

--- Stack/StockSpan.java
+++ Stack/StockSpan.java
@@ -1,21 +1,34 @@
 import java.util.*;
 
 public class StockSpan {
+    static List<Integer> list = new ArrayList<>();
 
-    private static List<Integer> findStockSpan(int[] arr) 
+    StockSpan() {
+        list = new ArrayList<>();
+    }
+
+    private static int findStockSpan(int value) 
     {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (st.isEmpty()) {
+        list.add(value);
+        int count = 1;
+        for (int i = list.size() - 2; i >= 0; i--) {
+            if (list.get(i) <= value) {
+                count++;
+            } else {
+                break;
             }
-        }
+        } 
+        return count;
     }
     
-    public static void mian(String[] args)
+    public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
         int arr[] = {100, 80, 60, 70, 60, 75, 85};
-
-        findStockSpan(arr);
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            result = findStockSpan(arr[i]);
+            System.out.print(result + " ");
+        }
     }
 }
