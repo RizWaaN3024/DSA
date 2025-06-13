9f3d37d905479e0990c1d46eb99742bbb88f755b
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-13 13:21:55 +0530

    Stock Span Brute

--- Stack/StockSpanBrute.java
+++ Stack/StockSpanBrute.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
-public class StockSpan {
+public class StockSpanBrute {
     static List<Integer> list = new ArrayList<>();
 
-    StockSpan() {
+    StockSpanBrute() {
         list = new ArrayList<>();
     }
 
