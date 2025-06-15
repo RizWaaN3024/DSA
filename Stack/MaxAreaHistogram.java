acdaa04ba379dcca747474510a467edae2fbfab3
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-16 02:55:16 +0530

    Histogram

--- Stack/MaxAreaHistogram.java
+++ Stack/MaxAreaHistogram.java
@@ -1,3 +1,38 @@
+import java.util.*;
+
 public class MaxAreaHistogram {
-    
+    public class Pair {
+        int value;
+        int index;
+
+        public Pair(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+    }
+    public int findMaxArea(int[] arr) 
+    {
+
+    }
+    public List<Integer> findNearestSmallerLeft(int[] arr) {
+        Stack<Pair> st = new Stack<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        int psuedoIndex = -1;
+        for (int i = 0; i < arr.length; i++) {
+            while (!st.isEmpty() && st.peek().value > arr[i]) {
+                st.pop();
+            }
+            if (st.isEmpty()) {
+                st.push(new Pair(0, psuedoIndex));
+            } else {
+                list.add(st.peek().index);
+            }
+            st.push(new Pair(arr[i], i));
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,1,5,6,2,3};
+        findMaxArea(arr);
+    }
 }
