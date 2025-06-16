dc20eab44558af8db96b7b1d6ec984bc345585f3
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-17 01:46:41 +0530

    Uodate

--- Stack/MaxAreaHistogramOptimised.java
+++ Stack/MaxAreaHistogramOptimised.java
@@ -1,3 +1,14 @@
+import java.util.*;
+
 public class MaxAreaHistogramOptimised {
-    
+    public static int findMaxAreaOfRectangle(int[] arr) {
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = { 6, 2, 5, 4, 5, 1, 6 };
+        int result = findMaxAreaOfRectangle(arr);
+        System.out.println(result);
+    }
 }
