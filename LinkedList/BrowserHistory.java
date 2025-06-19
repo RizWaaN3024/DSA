f1e155d6307a6c045d783aaa5690c80dd9c9a3f0
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-19 14:22:54 +0530

    Browser History

---
+++ LinkedList/BrowserHistory.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class BrowserHistory {
+    public class Node {
+        String data;
+        Node next;
+        Node prev;
+        public Node() {
+            this.data = "";
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public BrowserHistory(String url) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
