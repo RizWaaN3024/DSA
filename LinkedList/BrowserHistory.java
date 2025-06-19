111e95d50749ef88b6e300f9f2c6d464b8708507
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-19 18:14:28 +0530

    Browser History

--- LinkedList/BrowserHistory.java
+++ LinkedList/BrowserHistory.java
@@ -5,17 +5,45 @@ public class BrowserHistory {
         String data;
         Node next;
         Node prev;
-        public Node() {
-            this.data = "";
+        public Node(String url) {
+            this.data = url;
             this.next = null;
             this.prev = null;
         }
     }
-
-    public BrowserHistory(String url) {
-
+    Node current;
+    public BrowserHistory(String homePage) {
+        current = new Node(homePage);
     }
 
+    public void visit(String url) {
+        Node newSite = new Node(url);
+        current.next = newSite;
+        newSite.prev = current;
+        current = newSite;
+    }
+    
+    public String forward(int steps) {
+        while (steps-- > 0) {
+            if (current.next != null) {
+                current = current.next;
+            } else {
+                break;
+            }
+        }
+        return current.data;
+    }
+
+    public String back(int steps) {
+        while (steps-- > 0) {
+            if (current.prev != null) {
+                current = current.prev;
+            } else {
+                break;
+            }
+        }
+        return current.data;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
     }
