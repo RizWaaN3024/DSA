38852453bf31c0c6fb3fad4055baf710cacfa3c0
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-21 00:17:58 +0530

    Update

--- Recursion/PrintAllPermutationsApproach2.java
+++ Recursion/PrintAllPermutationsApproach2.java
@@ -1,3 +1,13 @@
+import java.util.*;
+
 public class PrintAllPermutationsApproach2 {
-    
+    public static void recurPermute(int index, int[] nums, List<List<Integer>> ans) {
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nums[] = {1, 2, 3};
+        List<List<Integer>> ans = new ArrayList<>();
+        recurPermute(0, nums, ans);
+    }
 }
