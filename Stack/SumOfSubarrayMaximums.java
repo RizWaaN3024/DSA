cc444667ea7b4fc689d87f2fe47113e7e8a7e1a6
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-21 14:54:59 +0530

    Sum of subarray maximums

--- Stack/SumOfSubarrayMaximums.java
+++ Stack/SumOfSubarrayMaximums.java
@@ -8,7 +8,7 @@ public class SumOfSubarrayMaximums {
         int[] pge = findPGE(arr);
         int total = 0;
         int mod = (int)(1e9 + 7);
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < n; i++) {
             int left = i - pge[i];
             int right = nge[i] - i;
             long contributions = ((long) left * right * arr[i]) % mod;
@@ -16,9 +16,45 @@ public class SumOfSubarrayMaximums {
         }
         return total;
     }
+
+    public static int[] findNGE(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        int n = arr.length;
+        int result[] = new int[n];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] <= arr[i]) {
+                st.pop();
+            }
+            if (st.isEmpty()) {
+                result[i] = n;
+            } else {
+                result[i] = st.peek();
+            }
+            st.push(i);
+        }
+        return result;
+    }
+
+    public static int[] findPGE(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        int n = arr.length;
+        int result[] = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            while (!st.isEmpty() && arr[st.peek()] < arr[i]) {
+                st.pop();
+            }
+            if (st.isEmpty()) {
+                result[i] = -1;
+            } else {
+                result[i] = st.peek();
+            }
+            st.push(i);
+        }
+        return result;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int arr[] = {3, 1, 2, 4};
+        int arr[] = {1, 2, 3};
         int result = findSumOfSubarrayMaximums(arr);
         System.out.println(result);
     }
