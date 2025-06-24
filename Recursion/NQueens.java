f3ffb23b6164aa08ed7532d62bd6367f22e5fdec
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-25 01:29:43 +0530

    N Queens

--- Recursion/NQueens.java
+++ Recursion/NQueens.java
@@ -1,3 +1,29 @@
+import java.util.*;
+
 public class NQueens {
-    
+    public static List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+            List<List<String>> result = new ArrayList<>();
+            dfs(0, board, result);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 4;
+        List<List<String>> queen = solveNQueens(n);
+        int i = 1;
+        for (List<String> it : queen) {
+            System.out.println("Arrangement " + i);
+            for (String s : it) {
+                System.out.println(s);
+            }
+            System.out.println();
+            i += 1;
+        }
+    }
 }
