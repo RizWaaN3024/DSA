b9b1064653a8b7cef1d6aef8a48b6d416d0f026f
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-25 01:53:43 +0530

    Remove K digits

--- Stack/RemoveKDigits.java
+++ Stack/RemoveKDigits.java
@@ -1,7 +1,38 @@
 import java.util.*;
 
 public class RemoveKDigits {
+
+    public static String removeKDigits(String num, int k) {
+        Stack<Character> st = new Stack<>();
+        if (k == num.length()) {
+            return "0";
+        }
+        for (int i = 0; i < num.length(); i++) {
+            while (!st.isEmpty() && k > 0 && st.peek() - '0' > num.charAt(i) - '0') {
+                st.pop();
+                k -= 1;
+            }
+            if (!st.isEmpty() || num.charAt(i) != '0')
+            st.push(num.charAt(i));
+        }
+        if (!st.isEmpty() && k > 0) {
+            st.pop();
+            k--;
+        }
+        if (st.isEmpty()) {
+            return "0";
+        }
+        StringBuilder result = new StringBuilder();
+        while (!st.isEmpty()) {
+            result.append(st.pop());
+        }
+        return result.reverse().toString();
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String num = "1432219";
+        int k = 3;
+        String result = removeKDigits(num, k);
+        System.out.println(result);
     }   
 }
