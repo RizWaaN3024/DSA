cad6d8eb8070e9512bea55dbcbc9ee02a8b6907e
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-26 02:12:53 +0530

    KOKO eating bananas

--- BinarySearch/KokoEatingBananas.java
+++ BinarySearch/KokoEatingBananas.java
@@ -1,5 +1,15 @@
 package BinarySearch;
+import java.util.*;
 
 public class KokoEatingBananas {
-    
+
+    public static int findMinEatingSpeed(int[] piles) {
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int piles[] = {3,6,7,11};
+        int minSpeed = findMinEatingSpeed(piles);
+        System.out.println(minSpeed);
+    }
 }
