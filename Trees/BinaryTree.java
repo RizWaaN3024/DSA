378e07be86834f26f0135de4c5021937bdc33f6c
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-27 01:04:59 +0530

    tree

--- Trees/BinaryTree.java
+++ Trees/BinaryTree.java
@@ -1,5 +1,21 @@
 package Trees;
 
+import java.util.Scanner;
+
 public class BinaryTree {
-    
+    TreeNode root;
+
+    public BinaryTree(int rootValue) {
+        root = new TreeNode(rootValue);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree(1);
+        tree.root.left = new TreeNode(2);
+        tree.root.right = new TreeNode(3);
+        tree.root.left.left = new TreeNode(4);
+        tree.root.right.right = new TreeNode(5);
+        System.out.println("Binary Tree Created");
+    }
 }
