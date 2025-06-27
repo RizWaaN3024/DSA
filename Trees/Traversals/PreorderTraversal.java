eb4bc08bac8c18c710cfaf3f4bfa9cc7b82c0b02
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-28 01:55:54 +0530

    Preorder Traversal

--- Trees/Traversals/PreorderTraversal.java
+++ Trees/Traversals/PreorderTraversal.java
@@ -1,5 +1,38 @@
 import java.util.*;
 
 public class PreorderTraversal {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
     
+    public static void preorderTraversal(Node root)
+    {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        System.out.println("Preorder Traversal");
+        preorderTraversal(root);
+        System.out.println();
+    }
 }
