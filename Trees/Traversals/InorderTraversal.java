0a4104cf5df1df1fc8403f9cf6ea385e474d4c35
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-28 02:13:56 +0530

    Inorder Traversal

--- Trees/Traversals/InorderTraversal.java
+++ Trees/Traversals/InorderTraversal.java
@@ -3,6 +3,37 @@ import java.util.*;
 public class InorderTraversal {
     
     static class TreeNode {
-        
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        inorderTraversal(root.left);
+        System.out.print(root.data + " ");
+        inorderTraversal(root.right);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        System.out.println("Inorder Traversal");
+        inorderTraversal(root);
+        System.out.println();
     }
 }
