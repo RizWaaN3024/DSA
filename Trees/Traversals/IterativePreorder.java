a4e7ea4366e367fb6d7e1d9815ada155e5b703cd
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-29 17:17:46 +0530

    Iterative

--- Trees/Traversals/IterativePreorder.java
+++ Trees/Traversals/IterativePreorder.java
@@ -1,3 +1,22 @@
+import java.util.*;
+
+
 public class IterativePreorder {
+
+    public static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
     
+    public static List<List<Integer>> findPreorder (TreeNode root) {
+        Stack<TreeNode> st = new Stack<>();
+        List<List<Integer>> result = new ArrayList<>();
+    }  
 }
