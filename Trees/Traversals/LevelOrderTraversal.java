7171baa4f73be2e1cb5391c16e0a00535ab693cd
RizWaaN3024 <rizwaanans786@gmail.com>
2025-06-29 16:43:13 +0530

    Level Order Traversal

--- Trees/Traversals/LevelOrderTraversal.java
+++ Trees/Traversals/LevelOrderTraversal.java
@@ -1,3 +1,39 @@
+import java.util.*;
+
 public class LevelOrderTraversal {
-    
+    public static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode (int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static List<List<Integer>> findLevelOrderTraversal (TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> list = new LinkedList<>();
+        if (root == null) {
+            return list;
+        }
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelNum = queue.size();
+            List<Integer> subList = new LinkedList<>();
+            for (int i = 0; i < levelNum; i++) {
+                if (queue.peek().left != null) {
+                    queue.offer(queue.peek().left);
+                }
+                if (queue.peek().right != null) {
+                    queue.offer(queue.peek().right);
+                }
+                subList.add(queue.poll().data);
+            }
+            list.add(subList);
+        }
+        return list;
+    }
 }
