7b4a9e14c0a4638880ea61a2c6961148a6d6215f
RizWaaN3024 <rizwaanans786@gmail.com>
2025-08-13 23:39:06 +0530

    Fix

--- Recursion/PrintSubsequencesWithSumK.java
+++ Recursion/PrintSubsequencesWithSumK.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
 public class PrintSubsequencesWithSumK {
-    
-    private static void PrintSubsequencesWithSumEqualToK(int index, ArrayList<Integer> current, int[] arr, int sum, int k)
-    {
+
+    private static void PrintSubsequencesWithSumEqualToK(int index, ArrayList<Integer> current, int[] arr, int sum,
+            int k) {
         if (index == arr.length) {
             if (k == sum) {
                 System.out.println(current);
@@ -22,7 +22,7 @@ public class PrintSubsequencesWithSumK {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int arr[] = {1, 2, 1};
+        int arr[] = { 1, 2, 1 };
         int sum = 0;
         ArrayList<Integer> list = new ArrayList<Integer>();
         System.out.println("Enter the value of K: ");
